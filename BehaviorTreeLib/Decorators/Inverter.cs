32a57034d452142647b5c2ae8385b187ddce9126
Alejandro Perez <alexperezalonso@gmail.com>
2015-02-01 21:25:05 +0100

    Inverter Decorator added

---
+++ BehaviorTreeLib/Decorators/Inverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BehaviorTreeLib.Decorators
+{
+    public class Inverter : Decorator
+    {
+        public Inverter()
+        {
+            _children = new List<BehaviorTreeNode>(1);
+        }
+
+        public Inverter(BehaviorTreeNode child)
+        {
+            _children = new List<BehaviorTreeNode>(1);
+            _children.Add(child);
+        }
+
+        public override void Init()
+        {
+            _children[0].Init();
+        }
+
+        public override BehaviorReturnCode Run()
+        {
+            BehaviorReturnCode status = _children[0].Run();
+            if (status == BehaviorReturnCode.BT_FAILURE)
+                return BehaviorReturnCode.BT_SUCCESS;
+            else if (status == BehaviorReturnCode.BT_SUCCESS)
+                return BehaviorReturnCode.BT_FAILURE;
+
+            return status;
+        }
+    }
+}
